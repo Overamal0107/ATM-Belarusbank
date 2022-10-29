26069fe6c469ff08453b0c17b3eea291efd5513b
OBITO <90102005+Vibe-OBITO@users.noreply.github.com>
2022-10-29 05:15:19 +0300

    Update project_Belarus_BANK.cpp

--- project_Belarus_BANK/project_Belarus_BANK.cpp
+++ project_Belarus_BANK/project_Belarus_BANK.cpp
@@ -1,28 +1,26 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
-#include <conio.h>
+#define _CRT_SECURE_NO_WARNINGS#include <conio.h>
 
 #include <iostream>
 
 #include "bank.h"
 
-using namespace std;
+#include <string>
 
+#include <cassert>
+
+using namespace std;
 
 int main()
 
 {
 
 	setlocale(LC_ALL, "Russian");
-
 	system("color 2");
 	system("CLS");
 	system("mode con cols=70 lines=50");
 
-
 	Bank::bank Bank_A;
 	if (Bank_A.User->New->Enter_to_your_account()) Bank_A.Menu();
 	return 0;
-  
-    
-}
 
+}
